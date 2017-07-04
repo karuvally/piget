7015016e819533859d47c8d58cf0e63184b5bb63
Aswin Karuvally <aswinbabuk@gmail.com>
2017-07-04 14:46:03 +0530

    added the retrieval code

--- README.md
+++ README.md
@@ -3,9 +3,14 @@ Automated File Retrieval tool
 
 ## Introduction 
 Piget is an FTP based automated file retrieval tool. 
-It retrieves entire remote directories periodically. 
+It retrieves entire remote directories periodically,
+then deletes the original files.
 
 ## Why?
 My home surveillance system is a Raspberry Pi having limited memory. 
 This is very handy, as it will automatically move the remote directory, 
 containing surveillance footage to my workstation.
+
+## Why not just use wget and cron? 
+Piget allows me to do checksum for each file, making sure it is not
+faulty before deleting the originals. Also, creating this was a fun experience
--- piget.py
+++ piget.py
@@ -6,26 +6,49 @@
 from ftplib import FTP
 
 
-# retrieve files from the server
-def retrieve_files(directory_path):
-	pass
+# the function where stuff happens
+def retrieve_files(ftp_connection, directory_path):
+	# set up some essential variables
+	file_type_list = []
+	file_dictionary = {}
+	
+	# get the file information
+	ftp_connection.cwd(directory_path)
+	ftp_connection.retrlines('LIST', file_type_list.append)
+	file_list = ftp_connection.nlst()
+	
+	# create a dictionary containing file information
+	for i in range(0, len(file_list)):
+		file_dictionary[file_list[i]] = file_type_list[i][0]
+	
+	# retrieve the files
+	for file in file_dictionary:
+		if(file_dictionary[file]) == 'd':
+			os.mkdir(file)
+			os.chdir(file)
+			retrieve_files(ftp_connection, file)
+		else:
+			ftp_connection.retrbinary('RETR' + file, open(file, 'wb').write)
+	
+	# move one level up the file system
+	ftp_connection.cwd('..') # debug
+	os.chdir('..')
 
 
 # the main function
 def main():
+	# essential stuff
+	server = 'karuvally.org'
+	directory_path = '/home/pi/downloads/'
+	username = 'pi'
+	password = 'karuvally'
+	
 	# set up the connection
-	ftp_connection = FTP('karuvally.org')
-	ftp_connection.login(user='pi', passwd='karuvally')
+	ftp_connection = FTP(server)
+	ftp_connection.login(user=username, passwd=password)
 	
-	# change to desired directory and list contents
-	ftp_connection.cwd('/home/pi')
-	file_list = ftp_connection.nlst()
-	
-	#print(file_list) #debug
-	
-	ftp_connection.retrbinary('RETR bak.zip', open('bak.zip', 'wb').write)
-	
-	# log off from the connected machine
+	# retrieve files and quit
+	retrieve_files(ftp_connection, directory_path)
 	ftp_connection.quit()
 
 
