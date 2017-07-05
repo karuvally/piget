e09aae84a48384b6003ee5db3ac1ba99ab76467a
Aswin Karuvally <aswinbabuk@gmail.com>
2017-07-05 11:52:40 +0530

    added checksum function

--- piget.py
+++ piget.py
@@ -4,6 +4,29 @@
 
 # import some serious stuff
 from ftplib import FTP
+from paramiko import client
+import os
+
+
+# create checksum for the remote files
+def create_remote_checksum(server, ssh_user, ssh_pass, file):
+	# declare some essential variables
+	remote_checksum = ""
+	
+	# initiate the connection
+	ssh_connection = client.SSHClient()
+	ssh_connection.set_missing_host_key_policy(client.AutoAddPolicy())
+	ssh_connection.connect(server, username=ssh_user, password=ssh_pass)
+	
+	# execute the command, receive output
+	stdin, stdout, stderr = ssh_connection.exec_command(file)
+	while not stdout.channel.exit_status_ready():
+		while stdout.channel.recv_ready():
+			remote_checksum += str(stdout.channel.recv(1024), 'utf8')
+	
+	# close connection and return checksum
+	ssh_connection.close()
+	return remote_checksum
 
 
 # the function where stuff happens
@@ -28,7 +51,7 @@ def retrieve_files(ftp_connection, directory_path):
 			os.chdir(file)
 			retrieve_files(ftp_connection, file)
 		else:
-			ftp_connection.retrbinary('RETR' + file, open(file, 'wb').write)
+			ftp_connection.retrbinary('RETR ' + file, open(file, 'wb').write)
 	
 	# move one level up the file system
 	ftp_connection.cwd('..') # debug
@@ -43,7 +66,7 @@ def main():
 	username = 'pi'
 	password = 'karuvally'
 	
-	# set up the connection
+	# set up the ftp connection
 	ftp_connection = FTP(server)
 	ftp_connection.login(user=username, passwd=password)
 	
